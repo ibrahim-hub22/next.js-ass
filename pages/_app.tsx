65b3b71e8ef91d95678f3153b12bdd6f9c506f60
minna <yakubuaminat94@gmail.com>
2025-10-27 00:17:55 +0100

    fix tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-import "@/app/globals.css";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient({
